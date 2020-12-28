b932264cdd0955b659a9fd7f8273fff3bf13d988
odoo之家 <odoo123@qq.com>
2020-12-28 12:34:09 +0800

    企业邮箱模块初始化

---
+++ __manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+#################################################################################
+# Author      : 珠海-老天 (<281388879@qq.com>)
+# Copyright(c): 2020-Present OuduSoft Inc.
+# All Rights Reserved.
+#################################################################################
+
+{
+    'name': 'laotianmail',
+    'summary': '老天的邮箱模块',
+    'version': '12.0.1.0',
+    'description': """老天的邮箱模块.""",
+    'author': '281388879@qq.com',
+    'support': '281388879@qq.com',
+    'category': 'website',
+    'website': "https://moc.ke.qq.com",
+    'depends': [],
+    'data': [
+        'views/jump_exmail_view.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
